2e00de79065a60b65d0467a143794e1e46828363
xueyh <xueyh@si-tech.com.cn>
2023-11-08 20:00:32 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import styles from '@/app/ui/home.modules.css';
 import Link from 'next/link';
 import {lusitana} from "@/app/ui/fonts";
 import Image from 'next/image';
