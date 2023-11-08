8ecb97d7b95f54b857e60bc78443dd16097dd60e
xueyh <xueyh@si-tech.com.cn>
2023-11-08 14:15:50 +0800

    1

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import {Inter, Lusitana, Noto_Sans} from 'next/font/google';
+import { Noto_Sans_S_Chinese } from '@next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
+
+export const notoSans=Noto_Sans({
+    weight: ['400', '700'],
+    subsets:['latin']
+})
+
